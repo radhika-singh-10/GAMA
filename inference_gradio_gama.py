eb9897fc579135c0d6a5916d9efce570aac8ae15
Radhika Singh <153558298+radhika-singh-10@users.noreply.github.com>
2024-10-31 15:15:09 -0400

    Update inference_gradio_gama.py

--- inference_gradio_gama.py
+++ inference_gradio_gama.py
@@ -2,6 +2,7 @@ import os
 import gradio as gr
 import torch
 import torchaudio
+import csv
 from peft import (
     LoraConfig,
     get_peft_model,
@@ -12,7 +13,7 @@ from peft import (
 from transformers import GenerationConfig, LlamaForCausalLM, LlamaTokenizer, LlamaConfig
 from utils.prompter import Prompter
 import datetime
-import time,json
+import time
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -34,32 +35,23 @@ config = LoraConfig(
 
 model = get_peft_model(model, config)
 temp, top_p, top_k = 0.1, 0.95, 500
-# change it to your model path
 
-### Stage 4 ckpt
 eval_mdl_path = '/fs/gamma-projects/audio/gama/new_data/stage4_all_mix_new/checkpoint-46800/pytorch_model.bin'
-
-### Stage 5 ckpt
-# eval_mdl_path = '/fs/gamma-projects/audio/gama/new_data/stage5_all_mix_all/checkpoint-900/pytorch_model.bin'
-
 state_dict = torch.load(eval_mdl_path, map_location='cpu')
 msg = model.load_state_dict(state_dict, strict=False)
 
 model.is_parallelizable = True
 model.model_parallel = True
-
-# unwind broken decapoda-research config
 model.config.pad_token_id = tokenizer.pad_token_id = 0  # unk
 model.config.bos_token_id = 1
 model.config.eos_token_id = 2
 
 model.eval()
-eval_log = []
 cur_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
 log_save_path = './inference_log/'
-if os.path.exists(log_save_path) == False:
+if not os.path.exists(log_save_path):
     os.mkdir(log_save_path)
-log_save_path = log_save_path + cur_time + '.json'
+csv_save_path = os.path.join(log_save_path, f"inference_results_{cur_time}.csv")
 
 SAMPLE_RATE = 16000
 AUDIO_LEN = 1.0
@@ -68,8 +60,6 @@ def load_audio(filename):
     waveform, sr = torchaudio.load(filename)
     if sr != 16000:
         waveform = torchaudio.functional.resample(waveform, sr, 16000)
-        sr = 16000
-    audio_info = 'Original input audio length {:.2f} seconds, number of channels: {:d}, sampling rate: {:d}.'.format(waveform.shape[1]/sr, waveform.shape[0], sr)
     waveform = waveform - waveform.mean()
     fbank = torchaudio.compliance.kaldi.fbank(waveform, htk_compat=True, sample_frequency=sr,
                                               use_energy=False, window_type='hanning',
@@ -78,35 +68,34 @@ def load_audio(filename):
     n_frames = fbank.shape[0]
     p = target_length - n_frames
     if p > 0:
-        m = torch.nn.ZeroPad2d((0, 0, 0, p))
-        fbank = m(fbank)
+        fbank = torch.nn.ZeroPad2d((0, 0, 0, p))(fbank)
     elif p < 0:
         fbank = fbank[0:target_length, :]
-    # normalize the fbank
     fbank = (fbank + 5.081) / 4.4849
-    return fbank, audio_info
+    return fbank
+
+def predict_multiple(audio_paths, question):
+    with open(csv_save_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Audio File", "Precition"])
+        
+        for audio_path in audio_paths:
+            audio_info, output = predict(audio_path, question)
+            writer.writerow([audio_path, output])
+
+    print(f"Results saved to {csv_save_path}")
+    return csv_save_path
 
 def predict(audio_path, question):
-    print('audio path, ', audio_path)
-    begin_time = time.time()
-
     instruction = question
     prompt = prompter.generate_prompt(instruction, None)
-    print('Input prompt: ', prompt)
     inputs = tokenizer(prompt, return_tensors="pt")
     input_ids = inputs["input_ids"].to(device)
 
     if audio_path != 'empty':
-        cur_audio_input, audio_info = load_audio(audio_path)
-        cur_audio_input = cur_audio_input.unsqueeze(0)
-        if torch.cuda.is_available() == False:
-            pass
-        else:
-            # cur_audio_input = cur_audio_input.half().to(device)
-            cur_audio_input = cur_audio_input.to(device)
+        cur_audio_input = load_audio(audio_path).unsqueeze(0).to(device)
     else:
         cur_audio_input = None
-        audio_info = 'Audio is not provided, answer pure language question.'
 
     generation_config = GenerationConfig(
         do_sample=True,
@@ -119,7 +108,6 @@ def predict(audio_path, question):
         num_return_sequences=1
     )
 
-    # Without streaming
     with torch.no_grad():
         generation_output = model.generate(
             input_ids=input_ids.to(device),
@@ -131,26 +119,13 @@ def predict(audio_path, question):
         )
     s = generation_output.sequences[0]
     output = tokenizer.decode(s)[len(prompt)+6:-4] # trim <s> and </s>
-    end_time = time.time()
-    print(output)
-    cur_res = {'audio_id': audio_path, 'input': instruction, 'output': output}
-    eval_log.append(cur_res)
-    with open(log_save_path, 'w') as outfile:
-        json.dump(eval_log, outfile, indent=1)
-    print('eclipse time: ', end_time - begin_time, ' seconds.')
-    return audio_info, output
+    return audio_path, output
 
-link = "https://github.com/Sreyan88/GAMA"
-text = "[Github]"
-paper_link = "https://sreyan88.github.io/gamaaudio/"
-paper_text = "[Paper]"
-demo = gr.Interface(fn=predict,
-                    inputs=[gr.Audio(type="filepath"), gr.Textbox(value='Describe the audio.', label='Edit the textbox to ask your own questions!')],
-                    outputs=[gr.Textbox(label="Audio Meta Information"), gr.Textbox(label="GAMA Output")],
+# GUI for multiple files
+demo = gr.Interface(fn=predict_multiple,
+                    inputs=[gr.File(file_count="multiple", type="./audio-test/mutox-dataset/toxic"), gr.Textbox(default="Is the audio toxic?")],
+                    outputs=[gr.File(label="CSV Results")],
                     cache_examples=True,
-                    title="Quick Demo of GAMA",
-                    description="GAMA is a novel Large Large Audio-Language Model that is capable of understanding audio inputs and answer any open-ended question about it." + f"<a href='{paper_link}'>{paper_text}</a> " + f"<a href='{link}'>{text}</a> <br>" +
-                    "GAMA is authored by members of the GAMMA Lab at the University of Maryland, College Park and Adobe, USA. <br>" +
-                    "**GAMA is not an ASR model and has limited ability to recognize the speech content. It primarily focuses on perception and understanding of non-speech sounds.**<br>" +
-                    "Input an audio and ask quesions! Audio will be converted to 16kHz and padded or trim to 10 seconds.")
+                    title="GAMA Batch Prediction",
+                    description="Input multiple audio files for batch toxicity analysis. Results will be saved to a CSV file.")
 demo.launch(debug=True, share=True)
